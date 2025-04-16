e117769b91cd6f3180b83dda69fba36674a0a100
Intelligence Academy <124143628+Intelligence-Academy@users.noreply.github.com>
2025-04-16 22:29:17 +0000

    Changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,13 +2,15 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath(".."))  # so autodoc finds your package
+sys.path.insert(0, os.path.abspath(".."))  # so autodoc can import your package
 
-project = 'Intelligence Academy'
-author  = 'Intelligence Academy'
-release = '0.1'
-version = '0.1.0'
+# -- Project information -----------------------------------------------------
+project   = 'Intelligence Academy'
+author    = 'Intelligence Academy'
+release   = '0.1'
+version   = '0.1.0'
 
+# -- General configuration ---------------------------------------------------
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
@@ -17,17 +19,16 @@ extensions = [
     'sphinx.ext.viewcode',
     'myst_parser',
 ]
-
 autosummary_generate = True
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-}
 
-templates_path = ['_templates']
+intersphinx_mapping = {
+    'python':  ('https://docs.python.org/3/', None),
+    'sphinx':  ('https://www.sphinx-doc.org/en/master/', None),
+}
+templates_path   = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-# MyST (Markdown) support
+# -- MyST (Markdown) parser --------------------------------------------------
 myst_enable_extensions = [
     'deflist',
     'colon_fence',
@@ -35,31 +36,32 @@ myst_enable_extensions = [
     'html_image',
 ]
 
-# HTML output
-html_theme = 'pydata_sphinx_theme'
-html_static_path = ['_static']
+# -- HTML output -------------------------------------------------------------
+html_theme        = 'pydata_sphinx_theme'
+html_static_path  = ['_static']
+html_favicon      = '_static/favicon.ico'
+html_css_files    = ['css/custom.css']
+
 html_theme_options = {
     "logo": {
         "image_light": "logo-light.png",
-        "image_dark": "logo-dark.png",
+        "image_dark":  "logo-dark.png",
     },
-    "navbar_start": ["navbar-logo"],
+    "navbar_start":  ["navbar-logo"],
     "navbar_center": ["navbar-nav"],
-    "navbar_end": ["search-field.html", "navbar-icon-links"],
+    "navbar_end":    ["search-field.html", "navbar-icon-links"],
     "icon_links": [
         {
             "name": "GitHub",
-            "url": "https://github.com/yourorg/intelligence-academy",
+            "url":  "https://github.com/Intelligence-Academy/rtd-tutorial",
             "icon": "fab fa-github-square",
         },
         {
             "name": "PyPI",
-            "url": "https://pypi.org/project/intelligence-academy/",
+            "url":  "https://pypi.org/project/intelligence-academy/",
             "icon": "fab fa-python",
         },
     ],
-    "collapse_navigation": False,
+    "collapse_navigation":      False,
     "secondary_sidebar_items": ["page-toc", "edit-this-page", "sourcelink"],
 }
-html_css_files = ["css/custom.css"]
-html_favicon = "_static/favicon.ico"
